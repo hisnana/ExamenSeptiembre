ddb924a5c6450f13ebb2920cfd831a8a37036dd8
AnaArroyo <Tardes@GET-01>
2025-09-10 20:57:28 +0200

    2

--- src/ana/arroyoplaza/examen/Utilidades.java
+++ src/ana/arroyoplaza/examen/Utilidades.java
@@ -81,5 +81,117 @@ public class Utilidades {
 				System.out.println("El dato introducido no es correcto");
 			}
 	 }
-	
+	 
+	 public static void analisisTexto() {
+		 
+			//Parte 3
+			System.out.println("Introduce un texto: ");
+			Scanner scan = new Scanner(System.in);
+			scan = new Scanner(System.in);
+			String texto = scan.nextLine();
+			texto=Utilidades.normalizaEspacios(texto);
+			int totalVocales = 0;
+			int contadorEspacios = 0; 
+			
+			for (int i = 0 ; i < texto.length() ; i++) {
+				char letra =texto.trim().toLowerCase().charAt(i);
+				if (letra=='a'||letra=='e'||letra=='i'||letra=='o'||letra=='u') {
+					totalVocales++;
+				}
+				if (letra==' ') {
+					contadorEspacios++;
+				}
+				
+			}
+			
+//			if(texto.isEmpty()) {
+//				contadorEspacios=0;
+//			}
+			
+			System.out.println("Vocales="+totalVocales);
+			System.out.println("Paralabras="+contadorEspacios+1);
+			
+			for (int i = texto.length()-1 ; i > -1 ; i--) {
+				System.out.print(texto.charAt(i));
+			}
+			
+	 }
+	 
+	 public static void operacionesArray() {
+		//Parte 4
+			System.out.println();
+			System.out.println("Introduce el tamaño de un array: ");
+			Scanner scan = new Scanner(System.in);
+			scan = new Scanner(System.in);
+			
+			int m = scan.nextInt();
+			if (m>0) {
+				int[] array = new int[m];
+				
+				for(int i = 0; i < array.length ; i++) {
+					System.out.println("Intruduce el "+(i+1)+"º número");
+					scan = new Scanner(System.in);
+					int num = scan.nextInt();
+					
+				}
+				//Eliminar duplicados
+				int contador = 0;
+				for (int i = 0 ; i < m ; i ++) {
+					for (int j = 0 ; j < m ; j ++) {
+						if (i!=j&&array[i]!=array[j]) {
+							contador++;
+						}
+					}
+					
+				}
+				int[] sinDuplicados= new int[contador];
+				for (int i = 0 ; i < m ; i ++) {
+					for (int j = 0 ; j < contador ; j ++) {
+						if (array[i]!=sinDuplicados[j]) {
+							sinDuplicados[i]= array[i];
+						}
+					}
+					
+				}
+				System.out.println("SinDuplicados=");
+				for (int num : sinDuplicados) {
+					System.out.print(num+" ");
+				}
+				System.out.println();
+				System.out.println("Intruduce un número para ver si existe");
+				scan = new Scanner(System.in);
+				int k = scan.nextInt();
+				boolean existe = false;
+				System.out.println("Indices(k="+k+")=");
+				
+				for (int i = 0 ; i<m; i++) {
+					
+					if(k==array[i]) {
+						
+						System.out.print(+i+" ");
+						existe = true;
+					}
+				}
+				
+				if (existe==false) {
+					System.out.println("NO_ENCONTRADO");
+				}
+
+
+			}else {
+				System.out.println("El número debe ser mayor que 0");
+			}
+	 }
+	 
+	 public static void comprobarPrimo(){
+			//Parte 5
+			
+			System.out.println("Intruduce un número y te digo si es primo o no");
+			Scanner scan = new Scanner(System.in);
+			scan = new Scanner(System.in);
+			int num = scan.nextInt();
+			System.out.println("El número es primo="+Utilidades.esPrimo(num));
+			
+
+	 }
 }
